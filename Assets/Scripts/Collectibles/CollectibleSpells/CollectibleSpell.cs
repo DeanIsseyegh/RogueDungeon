7057724d21ce0d9fc694031fd6152f46969244e2
Dean Isseyegh <issdea@gmail.com>
2022-03-07 12:45:49 +0000

    Refactored spells to remove unnecessary spell prefabs

--- Assets/Scripts/Collectibles/CollectibleSpells/CollectibleSpell.cs
+++ Assets/Scripts/Collectibles/CollectibleSpells/CollectibleSpell.cs
@@ -1,14 +1,22 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Spell Collectible", menuName = "SpellCollectible")]
 public class CollectibleSpell : Collectible
 {
 
-    public Spell spell;
+    public SpellData spellData;
+    private GameObject _spellObj;
+
+    private void OnEnable()
+    {
+        _spellObj = new GameObject(spellData.name);
+    }
 
     public override void ApplyEffects(PlayerSpellManager playerSpellManager)
     {
-        var spellToLearn = Instantiate(spell);
+        Spell spellToLearn = _spellObj.AddComponent<PlayerSpell>();
+        spellToLearn.data = spellData;
         playerSpellManager.AddSpell(spellToLearn, icon);
     }
 
