205b88c6e73eefa6c33884452f394d0c1752ac37
Dean Isseyegh <issdea@gmail.com>
2022-03-07 13:06:06 +0000

    Refactored enemy spells to remove unnecessary enemy spell prefabs

--- Assets/Scripts/Enemy/Attack/RangedEnemyAttack.cs
+++ Assets/Scripts/Enemy/Attack/RangedEnemyAttack.cs
@@ -3,31 +3,31 @@ using UnityEngine.AI;
 
 public class RangedEnemyAttack : MonoBehaviour, EnemyAttack
 {
-    [SerializeField] private Spell spell;
     [SerializeField] private float attackDistance;
     
     private NavMeshAgent _navMeshAgent;
     private Animator _animator;
     private PlayerInventory _playerInventory;
+    private Spell _spell;
 
     private void Awake()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
         _animator = GetComponent<Animator>();
         _playerInventory = GameObject.FindWithTag("Player").GetComponent<PlayerInventory>();
-        spell = Instantiate(spell);
+        _spell = gameObject.GetComponent<EnemySpell>();
     }
 
     public void DoAttack()
     {
-        spell.Cast(_navMeshAgent, _animator, _playerInventory);
+        _spell.Cast(_navMeshAgent, _animator, _playerInventory);
     }
 
     public bool IsAttacking()
     {
         var currentAnimatorStateInfo = _animator.GetCurrentAnimatorStateInfo(0);
-        bool isSpellCastAnim = currentAnimatorStateInfo.IsName(spell.data.animationName);
-        return spell.IsCastingSpell || isSpellCastAnim;
+        bool isSpellCastAnim = currentAnimatorStateInfo.IsName(_spell.data.animationName);
+        return _spell.IsCastingSpell || isSpellCastAnim;
     }
 
     public float AttackDistance()
