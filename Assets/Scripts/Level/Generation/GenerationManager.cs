fc9c915901e9aa1c12e7ff842e76e2b5182fcdf5
Dean Isseyegh <issdea@gmail.com>
2022-04-18 12:54:43 +0100

    Refactor random room generator data into its own class

--- Assets/Scripts/Level/Generation/GenerationManager.cs
+++ Assets/Scripts/Level/Generation/GenerationManager.cs
@@ -1,21 +1,18 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 public class GenerationManager : MonoBehaviour
 {
     [SerializeField] private Vector3 startingPos;
+    [SerializeField] private RandomRoomDataGenerator randomRoomDataGenerator;
     [SerializeField] private RoomGenerator roomGenerator;
-    [SerializeField] private List<RoomData> roomsData;
-
-    private bool hasGenerationStarted;
+    [SerializeField] private List<RoomData> testRoomData;
 
     void Start()
     {
-        GenerateRandomRooms();
+        List<RoomData> roomData = randomRoomDataGenerator.GenerateRoomData();
+        GenerateRooms(roomData);
     }
 
     public void GenerateRooms(List<RoomData> roomsData)
@@ -34,121 +31,5 @@ public class GenerationManager : MonoBehaviour
         }
 
         generatedRooms.ForEach(room => room.BuildNavmesh());
-    }
-
-    //Todo refactor this - just prototype for now
-    public void GenerateRandomRooms()
-    {
-        int numOfMainRooms = Random.Range(5, 9);
-        int initialEnemyCounter = 2;
-        int enemyCounterIncreasePerRoom = 2;
-
-        List<RoomData> roomData = new List<RoomData>();
-
-        Func<int> sizeRangeHallwayRoom = () =>
-        {
-            int[] possibleSizes = {3, 5};
-            int randomIndex = Random.Range(0, possibleSizes.Length);
-            return possibleSizes[randomIndex];
-        };
-
-        Func<int> sizeRangeEnemyRoom = () =>
-        {
-            int[] possibleSizes = {5, 7, 9};
-            int randomIndex = Random.Range(0, possibleSizes.Length);
-            return possibleSizes[randomIndex];
-        };
-        
-        Func<int> xSizePuzzleRoom = () =>
-        {
-            int[] possibleSizes = {5, 7};
-            int randomIndex = Random.Range(0, possibleSizes.Length);
-            return possibleSizes[randomIndex];
-        };
-        
-        Func<int> zSizePuzzleRoom = () =>
-        {
-            int[] possibleSizes = {3, 5};
-            int randomIndex = Random.Range(0, possibleSizes.Length);
-            return possibleSizes[randomIndex];
-        };
-
-
-        RoomData firstRoom = ScriptableObject.CreateInstance<RoomData>();
-        firstRoom.hasEntrance = false;
-        firstRoom.hasExit = true;
-        firstRoom.hasSpell = true;
-        firstRoom.xSize = sizeRangeHallwayRoom.Invoke();
-        firstRoom.zSize = sizeRangeHallwayRoom.Invoke();
-        roomData.Add(firstRoom);
-
-        RoomData prevRoom = firstRoom;
-        RoomData prevHallwayRoom = firstRoom;
-        int numOfEnemyRooms = 0;
-        for (int i = 1; i < numOfMainRooms; i++)
-        {
-            // Main Rooms
-            RoomData newRoom = ScriptableObject.CreateInstance<RoomData>();
-            roomData.Add(newRoom);
-            newRoom.hasEntrance = true;
-            newRoom.hasExit = i != numOfMainRooms - 1;
-            if (prevRoom.hasSpell || prevRoom.hasItem)
-            {
-                newRoom.hasEnemies = true;
-                newRoom.numberOfEnemies = initialEnemyCounter + (numOfEnemyRooms++ * enemyCounterIncreasePerRoom);
-                newRoom.xSize = sizeRangeEnemyRoom.Invoke();
-                newRoom.zSize = sizeRangeEnemyRoom.Invoke();
-            }
-            else
-            {
-                if (prevHallwayRoom.hasItem)
-                {
-                    newRoom.hasSpell = true;
-                }
-                else
-                {
-                    newRoom.hasItem = true;
-                }
-
-                newRoom.xSize = sizeRangeHallwayRoom.Invoke();
-                newRoom.zSize = sizeRangeHallwayRoom.Invoke();
-                prevHallwayRoom = newRoom;
-            }
-
-            // Side Rooms
-            if (newRoom.hasEnemies)
-            {
-                bool doesHaveRightSideRoom = 0 == Random.Range(0, 2);
-                bool doesHaveLeftSideRoom = 0 == Random.Range(0, 2);
-                if (doesHaveLeftSideRoom)
-                {
-                    newRoom.hasLeftSideRoom = true;
-                    newRoom.leftSideRoomData = GenerateRandomSideRoom(xSizePuzzleRoom, zSizePuzzleRoom);
-                }
-            
-                if (doesHaveRightSideRoom)
-                {
-                    newRoom.hasRightSideRoom = true;
-                    newRoom.rightSideRoomData = GenerateRandomSideRoom(xSizePuzzleRoom, zSizePuzzleRoom);
-                }
-            }
-            
-            prevRoom = newRoom;
-        }
-
-        GenerateRooms(roomData);
-    }
-
-    private static RoomData GenerateRandomSideRoom(Func<int> xSizePuzzleRoom, Func<int> zSizePuzzleRoom)
-    {
-        RoomData newSideRoom = ScriptableObject.CreateInstance<RoomData>();
-        newSideRoom.isPuzzleRoom = true;
-        newSideRoom.isBalloonRoom = 0 == Random.Range(0, 2);
-        newSideRoom.isSkullRoom = !newSideRoom.isBalloonRoom;
-        newSideRoom.xSize = xSizePuzzleRoom.Invoke();
-        newSideRoom.zSize = zSizePuzzleRoom.Invoke();
-        newSideRoom.hasEntrance = true;
-        newSideRoom.hasExit = false;
-        return newSideRoom;
     }
 }
---
+++ Assets/Scripts/Level/Generation/RandomRoomDataGenerator.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class RandomRoomDataGenerator : MonoBehaviour
+{
+
+    [Title("Hallway")] 
+    [SerializeField] private int[] hallWayXSizes = {3, 5};
+    [SerializeField] private int[] hallWayZSizes = {3, 5};
+    
+    [Title("Enemy Room")] 
+    [SerializeField] private int[] enemyRoomXSizes = {5, 7, 9};
+    [SerializeField] private int[] enemyRoomZSizes = {5, 7, 9};
+    
+    [Title("Puzzle Room")] 
+    [SerializeField] private int[] puzzleRoomXSizes = {5, 7};
+    [SerializeField] private int[] puzzleRoomZSizes = {3, 5};
+
+    private static int GenerateRandomValue(int[] possibleValues)
+    {
+        int randomIndex = Random.Range(0, possibleValues.Length);
+        return possibleValues[randomIndex];
+    }
+
+    public List<RoomData> GenerateRoomData()
+    {
+        int numOfMainRooms = Random.Range(5, 9);
+        int initialEnemyCounter = 2;
+        int enemyCounterIncreasePerRoom = 2;
+
+        List<RoomData> roomData = new List<RoomData>();
+
+        Func<int> xSizeRangeHallwayRoom = () => GenerateRandomValue(hallWayXSizes);
+        Func<int> zSizeRangeHallwayRoom = () => GenerateRandomValue(hallWayZSizes);
+
+        Func<int> xSizeRangeEnemyRoom = () => GenerateRandomValue(enemyRoomXSizes);
+        Func<int> zSizeRangeEnemyRoom = () => GenerateRandomValue(enemyRoomZSizes);
+
+        Func<int> xSizePuzzleRoom = () => GenerateRandomValue(puzzleRoomXSizes);
+        Func<int> zSizePuzzleRoom = () => GenerateRandomValue(puzzleRoomZSizes);
+
+        RoomData firstRoom = ScriptableObject.CreateInstance<RoomData>();
+        firstRoom.hasEntrance = false;
+        firstRoom.hasExit = true;
+        firstRoom.hasSpell = true;
+        firstRoom.xSize = xSizeRangeHallwayRoom.Invoke();
+        firstRoom.zSize = zSizeRangeHallwayRoom.Invoke();
+        roomData.Add(firstRoom);
+
+        RoomData prevRoom = firstRoom;
+        RoomData prevHallwayRoom = firstRoom;
+        int numOfEnemyRooms = 0;
+        for (int i = 1; i < numOfMainRooms; i++)
+        {
+            // Main Rooms
+            RoomData newRoom = ScriptableObject.CreateInstance<RoomData>();
+            roomData.Add(newRoom);
+            newRoom.hasEntrance = true;
+            newRoom.hasExit = i != numOfMainRooms - 1;
+            if (prevRoom.hasSpell || prevRoom.hasItem)
+            {
+                newRoom.hasEnemies = true;
+                newRoom.numberOfEnemies = initialEnemyCounter + (numOfEnemyRooms++ * enemyCounterIncreasePerRoom);
+                newRoom.xSize = xSizeRangeEnemyRoom.Invoke();
+                newRoom.zSize = zSizeRangeEnemyRoom.Invoke();
+            }
+            else
+            {
+                if (prevHallwayRoom.hasItem)
+                {
+                    newRoom.hasSpell = true;
+                }
+                else
+                {
+                    newRoom.hasItem = true;
+                }
+
+                newRoom.xSize = xSizeRangeHallwayRoom.Invoke();
+                newRoom.zSize = zSizeRangeHallwayRoom.Invoke();
+                prevHallwayRoom = newRoom;
+            }
+
+            // Side Rooms
+            if (newRoom.hasEnemies)
+            {
+                bool doesHaveRightSideRoom = 0 == Random.Range(0, 2);
+                bool doesHaveLeftSideRoom = 0 == Random.Range(0, 2);
+                if (doesHaveLeftSideRoom)
+                {
+                    newRoom.hasLeftSideRoom = true;
+                    newRoom.leftSideRoomData = GenerateRandomSideRoom(xSizePuzzleRoom, zSizePuzzleRoom);
+                }
+            
+                if (doesHaveRightSideRoom)
+                {
+                    newRoom.hasRightSideRoom = true;
+                    newRoom.rightSideRoomData = GenerateRandomSideRoom(xSizePuzzleRoom, zSizePuzzleRoom);
+                }
+            }
+            
+            prevRoom = newRoom;
+        }
+
+        return roomData;
+    }
+    
+    private static RoomData GenerateRandomSideRoom(Func<int> xSizePuzzleRoom, Func<int> zSizePuzzleRoom)
+    {
+        RoomData newSideRoom = ScriptableObject.CreateInstance<RoomData>();
+        newSideRoom.isPuzzleRoom = true;
+        newSideRoom.isBalloonRoom = 0 == Random.Range(0, 2);
+        newSideRoom.isSkullRoom = !newSideRoom.isBalloonRoom;
+        newSideRoom.xSize = xSizePuzzleRoom.Invoke();
+        newSideRoom.zSize = zSizePuzzleRoom.Invoke();
+        newSideRoom.hasEntrance = true;
+        newSideRoom.hasExit = false;
+        return newSideRoom;
+    }
+}
