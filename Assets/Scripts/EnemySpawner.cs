c50e8b9890e59e555e3a53dec6c6e21f1e625bd0
Dean Isseyegh <issdea@gmail.com>
2022-04-28 17:17:02 +0100

    Refactor enemy spawner

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -14,40 +14,38 @@ public class EnemySpawner : MonoBehaviour
         Vector3 _desiredPosition = position;
         Vector3 posToStartSpawn = position + spawnStartOffset;
         GameObject createdEnemy = Instantiate(enemyPrefab, posToStartSpawn, quaternion);
-        createdEnemy.GetComponent<NavMeshAgent>().enabled = false;
-        createdEnemy.GetComponent<Collider>().enabled = false;
-        createdEnemy.GetComponent<EnemyAI>().enabled = false;
         GameObject healthBarCanvasObj = createdEnemy.GetComponentInChildren<Canvas>().gameObject;
-        healthBarCanvasObj.SetActive(false);
+        SetEnabledCoreComponents(createdEnemy, healthBarCanvasObj, false);
         List<GameObject> createdEffects = spawnEffects.Select(effect => Instantiate(effect, position, Quaternion.identity)).ToList();
         StartCoroutine(StartSpawning(_desiredPosition, createdEnemy, healthBarCanvasObj, createdEffects));
         return createdEnemy;
     }
-    
+
     private IEnumerator StartSpawning(Vector3 desiredPosition, GameObject createdEnemy, GameObject healthBarCanvasObj, List<GameObject> createdEffects)
     {
         while (true)
         {
-            Debug.Log("Starting Spawn");
             createdEnemy.transform.position =
                 Vector3.MoveTowards(createdEnemy.transform.position, desiredPosition, 1f * Time.deltaTime);
 
             if (Vector3.Distance(createdEnemy.transform.position, desiredPosition) < 0.1f)
             {
-                createdEnemy.GetComponent<NavMeshAgent>().enabled = true;
-                createdEnemy.GetComponent<Collider>().enabled = true;
-                createdEnemy.GetComponent<EnemyAI>().enabled = true;
-                healthBarCanvasObj.SetActive(true);
+                SetEnabledCoreComponents(createdEnemy,  healthBarCanvasObj, true);
                 createdEffects.ForEach(Destroy);
-                Debug.Log("Ending the Spawn");
                 yield break;
             }
 
             yield return new WaitForEndOfFrame();
-            Debug.Log("Waited for End Of Spawn");        
         }
     }
 
-    
+    private static void SetEnabledCoreComponents(GameObject createdEnemy, GameObject healthBarCanvasObj,
+        bool isEnabled)
+    {
+        createdEnemy.GetComponent<NavMeshAgent>().enabled = isEnabled;
+        createdEnemy.GetComponent<Collider>().enabled = isEnabled;
+        createdEnemy.GetComponent<EnemyAI>().enabled = isEnabled;
+        healthBarCanvasObj.SetActive(isEnabled);
+    }
 
 }
