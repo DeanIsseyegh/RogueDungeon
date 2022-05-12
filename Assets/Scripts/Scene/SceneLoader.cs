f2e4f4446a427272e8e2c35083c3d77aac790c32
Dean Isseyegh <issdea@gmail.com>
2022-05-12 23:08:59 +0100

    Make start screen working

--- Assets/Scripts/Scene/SceneLoader.cs
+++ Assets/Scripts/Scene/SceneLoader.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.ResourceManagement.ResourceProviders;
+using UnityEngine.SceneManagement;
 
 namespace Scene
 {
     public class SceneLoader : MonoBehaviour
     {
         [SerializeField] private AssetReference inGameScene;
+        [SerializeField] private AssetReference startScreenScene;
         private AsyncOperationHandle<SceneInstance> _handle;
         private bool unloaded;
 
@@ -17,9 +19,33 @@ namespace Scene
             DontDestroyOnLoad(gameObject);
         }
 
-        private void Start()
+        public void LoadMainGameScene()
         {
-            // Addressables.LoadSceneAsync(inGameScene, )
+            Addressables.LoadSceneAsync(inGameScene, LoadSceneMode.Single)
+                .Completed += SceneLoadCompleted;
+        }
+        
+        public void LoadMainGameSceneStandard()
+        {
+            SceneManager.LoadScene("ThirdPersonScene");
+        }
+
+        private void SceneLoadCompleted(AsyncOperationHandle<SceneInstance> obj)
+        {
+            if (obj.Status == AsyncOperationStatus.Succeeded)
+            {
+                Debug.Log("Successfully loaded scene");
+                _handle = obj;
+            }
+        }
+
+        void UnloadScene()
+        {
+            Addressables.UnloadSceneAsync(_handle, true).Completed += op =>
+            {
+                if (op.Status == AsyncOperationStatus.Succeeded)
+                    Debug.Log("Successfully unloaded scene");
+            };
         }
     }
 }
